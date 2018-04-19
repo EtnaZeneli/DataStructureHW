47d6b366d22de36f9c2632b7fda7904d1f499301
EtnaZeneli <35538711+EtnaZeneli@users.noreply.github.com>
2018-04-18 22:07:41 -0400

    Add files via upload

---
+++ Final Project Checpoint2.py
@@ -0,0 +1,268 @@
+from tkinter import*
+import winsound
+root=Tk()
+root.geometry("500x400")
+frameleft=Frame(root)
+frameright=Frame(root)
+frametop=Frame(root)
+framebottom=Frame(root)
+frameleft.pack(side=LEFT)
+frameright.pack(side=RIGHT)
+frametop.pack(side=TOP)
+framebottom.pack(side=BOTTOM)
+
+def backbtn():
+    SearchaBar= Label(root, text="Search")
+    Searchbar=Entry(root)
+SearchaBar= Label(root, text="Search")
+Searchbar=Entry(root)
+def MusicCategories():
+    Seachbare= Searchbar.get()
+    if (Seachbare== "Jazz"):
+        def playsong0():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong1():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong2():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong3():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong4():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong5():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong6():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong7():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong8():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong9():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong10():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong11():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong12():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong13():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong14():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong15():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong16():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong17():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        lbl1 = Label (frameleft, text = "List of Jazz Music", height = 2, width = 15)
+        btn0 = Button(frameleft, text="Jazz", height = 1 ,width=5, command = playsong0)
+        btn1 = Button(frameleft, text="Play", height = 2, width=5, command = playsong1)
+        btn2 = Button(frameleft, text="Hip-Hop", height = 1, width=11, command = playsong2)
+        btn3 = Button(frameleft, text="Play", height= 2, width=5, command = playsong3)
+        btn4 = Button(frameleft, text="Classical Music", height=1, width=15, command = playsong4)
+        btn5 = Button(frameleft, text="Play", height=2, width=5, command = playsong5)
+        btn6 = Button(frameleft, text="Mixed Music Generes", height = 1, width=15, command = playsong6)
+        btn7 = Button(frameleft, text="Play", height = 2, width=5, command = playsong7)
+        btn8 = Button(frameright, text="Shuffle Button", height = 2, width=10, command = playsong8)
+        btn9 = Button(frameleft, text="Jazz", height = 1 ,width=5, command = playsong9)
+        btn10 = Button(frameleft, text="Play", height = 2, width=5, command = playsong10)
+        btn11 = Button(frameleft, text="Hip-Hop", height = 1, width=11, command = playsong11)
+        btn12 = Button(frameleft, text="Play", height= 2, width=5, command = playsong12)
+        btn13 = Button(frameleft, text="Classical Music", height=1, width=15, command = playsong13)
+        btn14 = Button(frameleft, text="Play", height=2, width=5, command = playsong14)
+        btn15 = Button(frameleft, text="Mixed Music Generes", height = 1, width=15, command = playsong15)
+        btn16 = Button(frameleft, text="Play", height = 2, width=5, command = playsong16)
+        btn17 = Button(frameright, text="Shuffle Button", height = 2, width=10, command = playsong17)
+        bckbtn= Button(frameright, text="Back", height = 2, width=10, command = backbtn)
+        lbl1.pack() 
+        btn0.pack() 
+        btn1.pack() 
+        btn2.pack() 
+        btn3.pack() 
+        btn4.pack() 
+        btn5.pack() 
+        btn6.pack() 
+        btn7.pack() 
+        btn8.pack() 
+        btn9.pack() 
+        btn10.pack() 
+        btn11.pack() 
+        btn12.pack() 
+        btn13.pack() 
+        btn14.pack() 
+        btn15.pack() 
+        btn16.pack() 
+        btn17.pack()
+        bckbtn.pack()
+    elif(Seachbare=="HipHop"):
+        
+        def playsong01():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong11():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong21():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong31():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong41():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong51():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong61():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong71():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong81():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong91():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong101():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong111():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong121():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong131():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong141():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong151():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong161():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong171():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        lbl11 = Label (frameleft, text = "List of Jazz Music", height = 2, width = 15)
+        btn01 = Button(frameleft, text="Jazz", height = 1 ,width=5, command = playsong01)
+        btn11 = Button(frameleft, text="Play", height = 2, width=5, command = playsong11)
+        btn21 = Button(frameleft, text="Hip-Hop", height = 1, width=11, command = playsong21)
+        btn31 = Button(frameleft, text="Play", height= 2, width=5, command = playsong31)
+        btn41 = Button(frameleft, text="Classical Music", height=1, width=15, command = playsong41)
+        btn51 = Button(frameleft, text="Play", height=2, width=5, command = playsong51)
+        btn61 = Button(frameleft, text="Mixed Music Generes", height = 1, width=15, command = playsong61)
+        btn71 = Button(frameleft, text="Play", height = 2, width=5, command = playsong71)
+        btn81 = Button(frameright, text="Shuffle Button", height = 2, width=10, command = playsong81)
+        btn91 = Button(frameleft, text="Jazz", height = 1 ,width=5, command = playsong91)
+        btn101 = Button(frameleft, text="Play", height = 2, width=5, command = playsong101)
+        btn111 = Button(frameleft, text="Hip-Hop", height = 1, width=11, command = playsong111)
+        btn121 = Button(frameleft, text="Play", height= 2, width=5, command = playsong121)
+        btn131 = Button(frameleft, text="Classical Music", height=1, width=15, command = playsong131)
+        btn141 = Button(frameleft, text="Play", height=2, width=5, command = playsong141)
+        btn151 = Button(frameleft, text="Mixed Music Generes", height = 1, width=15, command = playsong151)
+        btn161 = Button(frameleft, text="Play", height = 2, width=5, command = playsong161)
+        btn171 = Button(frameright, text="Shuffle Button", height = 2, width=10, command = playsong171)
+        bckbtn1= Button(frameright, text="Back", height = 2, width=10, command = backbtn)
+
+        lbl11.pack() 
+        btn01.pack() 
+        btn11.pack() 
+        btn21.pack() 
+        btn31.pack() 
+        btn41.pack() 
+        btn51.pack() 
+        btn61.pack() 
+        btn71.pack() 
+        btn81.pack() 
+        btn91.pack() 
+        btn101.pack() 
+        btn111.pack() 
+        btn121.pack() 
+        btn131.pack() 
+        btn141.pack() 
+        btn151.pack() 
+        btn161.pack() 
+        btn171.pack()
+        bckbtn1.pack()
+        
+    elif(Seachbare=="International"):
+        
+        def playsong02():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong12():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong22():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong32():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong42():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong52():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong62():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong72():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong82():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong92():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong102():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong112():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong122():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong132():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong142():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong152():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong162():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        def playsong172():
+            winsound.PlaySound('musical001.wav',winsound.SND_FILENAME)
+        lbl12 = Label (frameleft, text = "List of Jazz Music", height = 2, width = 15)
+        btn02 = Button(frameleft, text="Jazz", height = 1 ,width=5, command = playsong02)
+        btn12 = Button(frameleft, text="Play", height = 2, width=5, command = playsong12)
+        btn22 = Button(frameleft, text="Hip-Hop", height = 1, width=11, command = playsong22)
+        btn32 = Button(frameleft, text="Play", height= 2, width=5, command = playsong32)
+        btn42 = Button(frameleft, text="Classical Music", height=1, width=15, command = playsong42)
+        btn52 = Button(frameleft, text="Play", height=2, width=5, command = playsong52)
+        btn62 = Button(frameleft, text="Mixed Music Generes", height = 1, width=15, command = playsong62)
+        btn72 = Button(frameleft, text="Play", height = 2, width=5, command = playsong72)
+        btn82 = Button(frameright, text="Shuffle Button", height = 2, width=10, command = playsong82)
+        btn92 = Button(frameleft, text="Jazz", height = 1 ,width=5, command = playsong92)
+        btn102 = Button(frameleft, text="Play", height = 2, width=5, command = playsong102)
+        btn112 = Button(frameleft, text="Hip-Hop", height = 1, width=11, command = playsong112)
+        btn122 = Button(frameleft, text="Play", height= 2, width=5, command = playsong122)
+        btn132 = Button(frameleft, text="Classical Music", height=1, width=15, command = playsong132)
+        btn142 = Button(frameleft, text="Play", height=2, width=5, command = playsong142)
+        btn152 = Button(frameleft, text="Mixed Music Generes", height = 1, width=15, command = playsong152)
+        btn162 = Button(frameleft, text="Play", height = 2, width=5, command = playsong162)
+        btn172 = Button(frameright, text="Shuffle Button", height = 2, width=10, command = playsong172)
+        bckbtn2= Button(frameright, text="Back", height = 2, width=10, command = backbtn)
+
+        lbl12.pack() 
+        btn02.pack() 
+        btn12.pack() 
+        btn22.pack() 
+        btn32.pack() 
+        btn42.pack() 
+        btn52.pack() 
+        btn62.pack() 
+        btn72.pack() 
+        btn82.pack() 
+        btn92.pack() 
+        btn102.pack() 
+        btn112.pack() 
+        btn122.pack() 
+        btn132.pack() 
+        btn142.pack() 
+        btn152.pack() 
+        btn162.pack() 
+        btn172.pack()
+        bckbtn2.pack()
+    else:
+        lbl007= Label(root, text="No Data Found")
+        lbl007.pack()
+  
+btnsearch = Button (root, text = "Search Music", command= MusicCategories)
+btnsearch.pack()
+SearchaBar.pack()
+Searchbar.pack()
+
+
+mainloop()
+
